4c1965bce77c1b1048dc5decf89c5db0825980be
Biz3l <gabrieloliveiralveslegal@gmail.com>
2025-09-16 03:44:59 -0300

    Update: More easter eggs added

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const aboutMe = document.getElementById("titleRed");
 
 aboutMe.addEventListener("mouseenter", () => {
     document.title = "An EASTER EGG :))";
+    aboutMe.style.backgroundColor = "green";
+    aboutMe.style.transition = "background-color 0.5s"
 });
 
 aboutMe.addEventListener("mouseleave", () => {
     document.title = "Biz3l Portfolio Site";
+    aboutMe.style.backgroundColor = "";
 });
