3096963a5ddd3dcf241f2f24442ceee3a7e19463
Biz3l <gabrieloliveiralveslegal@gmail.com>
2025-09-16 04:15:03 -0300

    Update: Easter Egg Color

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const aboutMe = document.getElementById("titleRed");
 
 aboutMe.addEventListener("mouseenter", () => {
     document.title = "An EASTER EGG :))";
-    aboutMe.style.backgroundColor = "green";
+    aboutMe.style.backgroundColor = "4DFF93";
     aboutMe.style.transition = "background-color 0.5s"
 });
 
