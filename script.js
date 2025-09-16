58ae3d71d2f913f30563b6ed06eb7e05f940305d
Biz3l <gabrieloliveiralveslegal@gmail.com>
2025-09-16 02:15:25 -0300

    Git Create

---
+++ script.js
@@ -0,0 +1,9 @@
+const aboutMe = document.getElementById("titleRed");
+
+aboutMe.addEventListener("mouseenter", () => {
+    document.title = "An EASTER EGG :))";
+});
+
+aboutMe.addEventListener("mouseleave", () => {
+    document.title = "Biz3l Portfolio Site";
+});
