4ca3ede6f4c22ae0e727a5fc4376839f125ab56b
Biz3l <gabrieloliveiralveslegal@gmail.com>
2025-10-02 03:52:37 -0300

    Updates: Colors are now more animated :)

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ import { randomColorsBack } from "./Resources/Modules/ChangeColor.js";
 
 const aboutMe = document.getElementById("titleRed");
 
+let randomColorsInterval;
+
 aboutMe.addEventListener("mouseenter", () => {
     document.title = "An EASTER EGG :))";
+    randomColorsInterval = setInterval(() => {
     randomColorsBack(aboutMe);
-    aboutMe.style.transition = "background-color 0.5s";
-    
+    aboutMe.style.transition = "background-color 0.3s";
+    }, 375);
 });
 
 aboutMe.addEventListener("mouseleave", () => {
+    clearInterval(randomColorsInterval);
     document.title = "Biz3l Portfolio Site";
     aboutMe.style.backgroundColor = "";
 });
